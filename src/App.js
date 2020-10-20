b745da7ed1209a6ffadeac860f244673753d3f81
tyonek <tyonek.thompson@gmail.com>
2020-10-19 23:47:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component{
+ render(){
+   return(
+     <div>
+       <Switch>
+         <Route exact path={'/'} component={LandingPage}></Route>
+         <Route path={'/howdealer'} component={HowDealer}></Route>
+         <Route path={'/howbroker'} component={HowBroker}></Route>
+         <Route path={'/howtransporter'} component={HowTransporter}></Route>
+         <Route path={'/signup'} component={SignUp}></Route>
+         <Route path={'/login'} component={Login}></Route>
+         <Route path={'/dealer'} compomnent={Dealer}></Route>
+         <Route path={'/broker'} component={Broker}></Route>
+         <Route path={'/transporter'} component={Transporter}></Route>
+       </Switch>
+     </div>
+   )
+ }
 }
 
 export default App;
