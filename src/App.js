5cc276586bdd935467e348d75cca339942cd627c
tyonek <tyonek.thompson@gmail.com>
2020-10-20 18:20:38 -0400

    added imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import LandingPage from './routes/LandingPage/LandingPage';
+import HowBroker from './routes/AccountInfo/HowDealer/HowDealer';
+import HowDealer from './routes/AccountInfo/HowBroker/HowBroker';
+import HowTransporter from './routes/AccountInfo/HowTransporter/HowTransporter';
+import SignUp from './routes/Signup/Signup';
+import Login from './routes/Login/Login';
+import Dealer from './routes/UserAccounts/Dealer/Dealer';
+import Broker from './routes/UserAccounts/Broker/Broker';
+import Transporter from './routes/UserAccounts/Transporter/Transporter';
 export default class App extends Component{
  render(){
    return(
@@ -20,6 +28,4 @@ export default class App extends Component{
      </div>
    )
  }
-}
-
-export default App;
+};
